0faa086b44851574446882a4fe7191ec3b60e3b7
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2021-04-28 16:37:44 -0300

    Subject: add sort methods to home page Problem: Solution: Note:

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:crudnex/blocs/product/product_bloc.dart';
 import 'package:crudnex/blocs/product/product_events.dart';
 import 'package:crudnex/blocs/product/product_state.dart';
+import 'package:crudnex/data/models/product_model.dart';
 import 'package:crudnex/ui/widgets/custom_error_widget.dart';
 import 'package:crudnex/ui/widgets/loading_widget.dart';
 import 'package:crudnex/ui/widgets/product_card.dart';
@@ -15,11 +16,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ProductBloc productBloc = Modular.get<ProductBloc>();
+  var _orderBy;
+
+  int _orderByAlphabetical(a, b) {
+    return a.name.compareTo(b.name);
+  }
+
+  int _orderByPrice(a, b) {
+    return a.price.compareTo(b.price);
+  }
+
+  int _orderByUpdateAt(a, b) {
+    return a.updatedAt.compareTo(b.updatedAt);
+  }
 
   @override
   void initState() {
     super.initState();
     productBloc.add(FetchProduct());
+    _orderBy = _orderByAlphabetical;
   }
 
   @override
@@ -31,12 +46,35 @@ class _HomePageState extends State<HomePage> {
           title: Text("Pagina inicial"),
           actions: [
             Container(
-                margin: EdgeInsets.all(8), child: Icon(Icons.sort_by_alpha)),
+                margin: EdgeInsets.all(8),
+                child: IconButton(
+                  icon: Icon(Icons.sort_by_alpha),
+                  onPressed: () {
+                    setState(() {
+                      _orderBy = _orderByAlphabetical;
+                    });
+                  },
+                )),
             Container(
                 margin: EdgeInsets.all(8),
-                child: Icon(Icons.monetization_on_rounded)),
+                child: IconButton(
+                  icon: Icon(Icons.monetization_on_rounded),
+                  onPressed: () {
+                    setState(() {
+                      _orderBy = _orderByPrice;
+                    });
+                  },
+                )),
             Container(
-                margin: EdgeInsets.all(8), child: Icon(Icons.access_time)),
+                margin: EdgeInsets.all(8),
+                child: IconButton(
+                  icon: Icon(Icons.access_time),
+                  onPressed: () {
+                    setState(() {
+                      _orderBy = _orderByUpdateAt;
+                    });
+                  },
+                )),
           ],
         ),
         body: Column(
@@ -63,6 +101,7 @@ class _HomePageState extends State<HomePage> {
                     child: BlocBuilder<ProductBloc, ProductState>(
                       builder: (context, state) {
                         if (state is ProductSuccess) {
+                          state.products.sort(_orderBy);
                           return ListView(
                             children: state.products
                                 .map((product) => Center(
